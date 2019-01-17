363a0833f1f84f31b2683f21111b5c9b4134a492
pholmes10 <pholmes10@gmail.com>
2019-01-17 10:36:04 -0600

    added washington post url and scrape

--- server.js
+++ server.js
@@ -36,14 +36,16 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.washingtonpost.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("div.headline").each(function(i, element) {
       // Save an empty result object
       var result = {};
+
+      console.log(result);
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
